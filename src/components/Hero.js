05c990b159f7d324e1c5e0288b769fc99aec34f0
Scott Gerweck <scottgerweck@gmail.com>
2021-09-18 12:44:56 -0700

    changed map library

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 function Hero() {
+    const heroContainerStyles = {
+        // position: "absolute",
+        right: "0",
+        bottom: "0",
+        minWidth: "100%",
+        minHeight: "100vh",
+        // transform: "translateX(calc((100% - 100vh) / 2 ))"
+    }
+    
     return (
-        <ReactPlayer
-            url="https://media.giphy.com/media/d6KYCCIOAdsWL4aNm4/giphy.mp4"
-            playing
-            loop
-            muted
-            height="100%"
-            width="100%"
-        />
+        <div style={heroContainerStyles}>
+            <ReactPlayer
+                url="https://media.giphy.com/media/d6KYCCIOAdsWL4aNm4/giphy.mp4"
+                playing
+                loop
+                muted
+                height="200%"
+                width="200%"
+            />
+        </div>
     );
 }
 
--- src/components/RiverComponent.js
+++ src/components/RiverComponent.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React from 'react';
 
-class River extends Component {
-
-  render() {
-    return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyCOvUaRzb0eiFrC93f9_JbPB7jMUmuHjwc" }}
-        >
-        </GoogleMapReact>
-      </div>
+function River() {
+    return(
+        <div>
+            The River: Welcome to the Calapooia River
+        </div>
     );
-  }
 }
 
 export default River;
