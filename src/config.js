78f4642c8ee92e808f9565f74ce38d65226cdbda
Scott Alan <scottalandev@gmail.com>
2021-11-11 01:31:19 -0800

    env variables

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const baseUrl = process.env.BASE_URL;
-export const baseUrl = 'https://localhost:6251/';
+export const baseUrl = process.env.REACT_APP_API_URL;
+// export const baseUrl = 'https://localhost:6251/';
