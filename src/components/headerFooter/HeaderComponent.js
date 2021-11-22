135b4e3d9a7b4299048e7eb8f2863ae0744ae446
Scott Alan <scottalandev@gmail.com>
2021-11-22 14:14:59 -0800

    updated HeaderComponent from class to functional, fixed toggler so closes correctly when link is clicked

--- src/components/headerFooter/HeaderComponent.js
+++ src/components/headerFooter/HeaderComponent.js
@@ -1,54 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import './header.css';
 
-class Header extends Component {
+function Header() {
+	const [isNavOpen, toggleNav] = useState(false);
 
-    constructor(props) {
-        super(props);
-
-        this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-            isNavOpen: false
-        };
-    }
-
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
-
-    render() {
-        return(
-            <Navbar dark className="bg-dark dark" sticky="top" expand="md">
-                <div className="container">
-                    <NavbarBrand className="mr-auto" href="/">The Calapooia River</NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNav} />
-                    <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar className="ml-auto">
-                            <NavItem>
-                                <NavLink className="nav-link" to="/home">Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/river">The River</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/exploreMap">Explore Map</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/resources">Resources</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/tripReports">Trip Reports</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </div>
-            </Navbar>
-        )
-    }
+    return(
+        <Navbar dark className="bg-dark dark" sticky="top" expand="md">
+            <div className="container">
+                <NavbarBrand className="mr-auto" href="/">The Calapooia River</NavbarBrand>
+                <NavbarToggler onClick={() => toggleNav(!isNavOpen)} />
+                <Collapse isOpen={isNavOpen} navbar>
+                    <Nav navbar className="ml-auto">
+                        <NavItem>
+                            <NavLink className="nav-link" to="/home" onClick={() => toggleNav(false)}>Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/river" onClick={() => toggleNav(false)}>The River</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/exploreMap" onClick={() => toggleNav(false)}>Explore Map</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/resources" onClick={() => toggleNav(false)}>Resources</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/tripReports" onClick={() => toggleNav(false)}>Trip Reports</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </div>
+        </Navbar>
+    )
 }
+
 
 export default Header;
